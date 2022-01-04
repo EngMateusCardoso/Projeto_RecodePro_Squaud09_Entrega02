21bfc1468f4ad1f4dc319604aadde527982a3dfc
Mateus Cardoso Oliveira <eng.mateuscardosooliveira@gmail.com>
2022-01-04 12:18:23 -0300

    ASP.NET core (MVC)
    Pasta com ASP.NET core (MVC)

---
+++ Amor_na_Pele_Squad09_Entrega02/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Amor_na_Pele_Squad09_Entrega02.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
